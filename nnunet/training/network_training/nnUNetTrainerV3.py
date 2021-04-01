ea5e6075d163b9d897637a52fef57cd701af00f1
alexpron <alexandre.pron@gmail.com>
2021-04-01 13:56:43 +0200

    UP: reformatting class and check with other files

--- nnunet/training/network_training/nnUNetTrainerV3.py
+++ nnunet/training/network_training/nnUNetTrainerV3.py
@@ -8,6 +8,7 @@ class nnUNetTrainerV3(nnUNetTrainerV2):
         super().__init__(plans_file, fold, output_folder, dataset_directory, batch_dice, stage, unpack_data,
                          deterministic, fp16)
         self.max_num_epochs = 5
+        # actually should not be necessary but what is the harm 
         self.initial_lr = 1e-2
         self.deep_supervision_scales = None
         self.ds_loss_weights = None
