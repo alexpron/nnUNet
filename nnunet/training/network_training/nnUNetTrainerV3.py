4d13674e5ce45e66712b96f792b5ffd77bfc901d
alexpron <alexandre.pron@gmail.com>
2021-04-01 13:55:28 +0200

    UP: reformatting class and check with other files

--- nnunet/training/network_training/nnUNetTrainerV3.py
+++ nnunet/training/network_training/nnUNetTrainerV3.py
@@ -2,7 +2,7 @@ from nnunet.training.network_training.nnUNetTrainerV2 import nnUNetTrainerV2
 
 
 class nnUNetTrainerV3(nnUNetTrainerV2):
-
+    """Reduce the number of max epoch to have convergence sooner"""
     def __init__(self, plans_file, fold, output_folder=None, dataset_directory=None, batch_dice=True, stage=None,
                  unpack_data=True, deterministic=True, fp16=False):
         super().__init__(plans_file, fold, output_folder, dataset_directory, batch_dice, stage, unpack_data,
@@ -11,7 +11,6 @@ class nnUNetTrainerV3(nnUNetTrainerV2):
         self.initial_lr = 1e-2
         self.deep_supervision_scales = None
         self.ds_loss_weights = None
-
         self.pin_memory = True
 
 
